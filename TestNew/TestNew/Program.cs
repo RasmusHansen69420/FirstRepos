cb10083b3cd7efa8a88dc84baeddbb40ead58a85
RasmusHansen69420 <rblh69115@edu.ucl.dk>
2024-11-26 14:22:02 +0100

    Back to normal

--- TestNew/TestNew/Program.cs
+++ TestNew/TestNew/Program.cs
@@ -6,8 +6,6 @@
         {
             Console.WriteLine("Hej med dig!");
             Console.WriteLine("Hello World!");
-            for (int i = 0; i < 100000; i++)
-                Console.WriteLine(i);
         }
     }
 }
